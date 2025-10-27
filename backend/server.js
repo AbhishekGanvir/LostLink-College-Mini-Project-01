0141794d2c032cf542cc0f5a248903ffec60d61a
Abhishek Ganvir <abhishekganvirr@gmail.com>
2025-10-27 14:40:12 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000",
+    "https://lostlink-z8pw.onrender.com"
+  ],
   credentials: true,
 }));
 
